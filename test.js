8e335b9cee6e60fd3e67ff18c7085badcc22cfba
NadyaByk <natik.byk28@gmail.com>
2024-03-15 20:31:58 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,29 @@
+// let result = ''
+// let i = 0
+
+// do {
+//   i = i + 1
+//   result = result + i
+// } while (i < 5)
+
+// console.log(result)
+// // expected: '12345'
+// const items = [
+//     { id: 111, name: 'Burger', price: 299 },
+//     { id: 222, name: 'Fries', price: 150 },
+//     { id: 333, name: 'Cola', price: 199 },
+//   ]
+  
+//   const newItem = { id: 4444, name: 'Hot dog', price: 99 }
+//   items.push(newItem)
+
+const index = string.indexOf('h')
+
+console.log(index) // -1
+
+if (index >= 0) {
+  console.log('exists')
+} else {
+  console.log('does not exists')
+}
+// output: does not exists
